3321f87de40462edd3a3b60b6f1339d8fab2b362
dimt <151682599+cloverdust@users.noreply.github.com>
2026-01-19 16:25:03 -0500

    werwer

--- Scramjet/uv.sw.js
+++
@@ -1,8 +0,0 @@
-importScripts('https://cdn.jsdelivr.net/gh/titaniumnetwork-dev/Ultraviolet-App/uv/uv.bundle.js');
-importScripts('https://cdn.jsdelivr.net/gh/titaniumnetwork-dev/Ultraviolet-App/uv/uv.config.js');
-
-const sw = new UVServiceWorker();
-
-self.addEventListener('fetch', event => {
-  event.respondWith(sw.fetch(event));
-});
